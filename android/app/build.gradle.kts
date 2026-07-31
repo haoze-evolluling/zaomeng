0384690d65fc03658cf3b76e7bebfdab550a5d3b
wkbin <wangkebin1997@gmail.com>
2026-07-31 17:48:39 +0800

    Release v1.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "top.wkbin.zaomeng"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
